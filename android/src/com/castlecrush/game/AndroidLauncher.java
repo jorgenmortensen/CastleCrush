b526166dd7eb7374eae7e21b0861a8fafa612021
Jorgen Mortensen <jorgen.bm96@gmail.com>
2018-03-09 21:22:10 +0100

    Initial commit

---
+++ android/src/com/castlecrush/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.castlecrush.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.castlecrush.game.CastleCrush;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CastleCrush(), config);
+	}
+}
