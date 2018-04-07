d576c807c4095f5229ac7f1fd757fed9b114b468
Erik Kjernlie <erik_kjernlie@hotmail.com>
2018-04-07 19:15:21 +0200

    might be a few bugs with the disappearing logo

--- core/src/states/menuStates/PlayMenu.java
+++ core/src/states/menuStates/PlayMenu.java
@@ -12,7 +12,13 @@ import components.GravityButton;
 import states.GameStateManager;
 import states.State;
 
+import static states.menuStates.StartMenuScreen.changed_logo;
+import static states.menuStates.StartMenuScreen.crushed;
+import static states.menuStates.StartMenuScreen.little_crushed;
+import static states.menuStates.StartMenuScreen.logo;
 import static states.menuStates.StartMenuScreen.startTime;
+import static states.menuStates.StartMenuScreen.with_u;
+import static states.menuStates.StartMenuScreen.without_castle;
 
 /**
  * Created by erikkjernlie on 05/04/2018.
@@ -31,8 +37,6 @@ public class PlayMenu extends State {
     private GravityButton btnSingle;
     private GravityButton btnMulti;
     private GravityButton btnLocal;
-    private Texture logo;
-
     private Button btnSound;
 
 
@@ -40,7 +44,6 @@ public class PlayMenu extends State {
 
     public PlayMenu(GameStateManager gsm) {
         super(gsm);
-        logo = new Texture("logo.png");
         //background1 = new Texture("background.png");
         makeMovingBackground();
         makeButtons();
@@ -156,17 +159,28 @@ public class PlayMenu extends State {
             xCoordBg2 = 0;
         }
 
+
         long time = TimeUtils.timeSinceMillis(startTime);
-        if (time > 3500 && time < 7000) {
-            logo = new Texture("logo_little_crushed.png");
-        } else if (time > 7000 && time < 10000) {
-            logo = new Texture("logo_crushed.png");
-        } else if (time > 10000 && time < 13000) {
-            logo = new Texture("logo_without_castle.png");
-        } else if (time > 13000) {
-            logo = new Texture("logo_with_u.png");
+        if (time > 3500 && little_crushed) {
+            logo = new Texture("logo_little_crushed.png");;;
+            little_crushed = false;
+            changed_logo = true;
+        } else if (time > 7000 && crushed) {
+            logo = new Texture("logo_crushed.png");;
+            crushed = false;
+            changed_logo = true;
+        } else if (time > 10000  && without_castle) {
+            logo = new Texture("logo_without_castle.png");;
+            without_castle = false;
+            changed_logo = true;
+        } else if (time > 13000 && with_u) {
+            logo = new Texture("logo_with_u.png");;
+            with_u = false;
+            changed_logo = true;
         }
     }
+
+
 
     @Override
     public void render(SpriteBatch sb) {
@@ -204,7 +218,7 @@ public class PlayMenu extends State {
     //dispose textures / buttons
     @Override
     public void dispose() {
-        logo.dispose();
+
     }
 
 
