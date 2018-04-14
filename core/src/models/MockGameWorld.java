1072efb32e8ff44f920ea60146c23c90eb777132
Luddeyo <ludviglijo@gmail.com>
2018-04-14 15:14:21 +0200

    Setting correlation between physicsbody and sprites correctly, still hampered by speedlimit

--- core/src/models/MockGameWorld.java
+++ core/src/models/MockGameWorld.java
@@ -33,7 +33,7 @@ public class MockGameWorld {
     static final float STEP_TIME = 1f / 60f;
     static final int VELOCITY_ITERATIONS = 6;
     static final int POSITION_ITERATIONS = 2;
-    static final float SCALE = 0.05f;
+    static final float SCALE = 0.25f;
     private World physicsWorld;
 
     private TextureAtlas textureAtlas;
@@ -66,15 +66,16 @@ public class MockGameWorld {
 
     private void generateBodies() {
         createGround();
-        createBox(100, 10, 60, 60);
-        //createBox(60,10, 30,50);
-      //  createBox(40,10, 20,20);
-       // createBox(Gdx.graphics.getWidth() - 300, 10, 60,40);
+        createBox(500, 30, 60, 60);
+        createBox(550,30, 30,30);
+        createBox(600,60, 20,20);
+        createBox(600,80, 20,20);
+      //  createBox(Gdx.graphics.getWidth() - 300, 10, 60,40);
 
-        createProjectile(20, 30, 15);
+        createProjectile(20, 20, 50);
     }
 
-    private void createGround() {
+    public void createGround() {
         if (body != null) physicsWorld.destroyBody(body);
 
         int groundHeight = 20;
@@ -83,10 +84,10 @@ public class MockGameWorld {
         bodyDef.type = BodyDef.BodyType.StaticBody;
 
         FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.friction = 0.01f;
+        fixtureDef.friction = 0.1f;
 
         PolygonShape shape = new PolygonShape();
-        shape.setAsBox(Gdx.graphics.getWidth()/PTM_RATIO,groundHeight/PTM_RATIO/2);
+        shape.setAsBox(Gdx.graphics.getWidth(),groundHeight/2);
         fixtureDef.shape = shape;
 
         body = physicsWorld.createBody(bodyDef);
@@ -97,7 +98,6 @@ public class MockGameWorld {
 
 
 
-        //Sprite groundSprite = new Sprite(new Texture("bottom_ground.png"));
         Sprite groundSprite = textureAtlas.createSprite("bottom_ground");
         //groundSprite.setScale(Gdx.graphics.getWidth(), 1);
         groundSprite.setSize(Gdx.graphics.getWidth(), groundHeight/2);
@@ -107,76 +107,23 @@ public class MockGameWorld {
 
 
     private void createBox(float xPos, float yPos, int width, int height){
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyDef.BodyType.DynamicBody;
-
-        bodyDef.position.set(xPos/PTM_RATIO, yPos/PTM_RATIO);
-
-        Body body = physicsWorld.createBody(bodyDef);
-
-        PolygonShape shape = new PolygonShape();
-        //*2 because the parameters is the "half width" of the box, from center og the box
-        shape.setAsBox(width/PTM_RATIO/2,height/PTM_RATIO/2);
-
-        FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.shape = shape;
-        fixtureDef.friction = 0.1f;
-        fixtureDef.density = 1;
-        fixtureDef.restitution = 0.3f;
-
-        Fixture fixture = body.createFixture(fixtureDef);
-
-        body.createFixture(fixtureDef);
-        body.setTransform(xPos/PTM_RATIO,yPos/PTM_RATIO,0);
-
-        shape.dispose();
         Sprite sprite = textureAtlas.createSprite("brick1");
-        sprite.setSize(width, height);
-
         body = createBody("brick1", xPos, yPos, 0);
-
-        mockBoxes.add(new Box(body, sprite, width, height));
+        sprite.setScale(SCALE);
+        sprite.setOrigin(0, 0);
+        mockBoxes.add(new Box(body, sprite, width*2, height*2));
 
     }
 
     private void createProjectile(float xPos, float yPos, int radius){
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyDef.BodyType.DynamicBody;
-
-        // Set our body's starting position in the world
-        bodyDef.position.set(xPos/PTM_RATIO, yPos/PTM_RATIO);
-
-// Create our body in the world using our body definition
-        Body body = physicsWorld.createBody(bodyDef);
-
-// Create a circle shape and set its radius to 6
-        CircleShape circle = new CircleShape();
-        circle.setRadius(radius/PTM_RATIO);
-
-// Create a fixture definition to apply our shape to
-        FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.shape = circle;
-        fixtureDef.density = 0.5f;
-        fixtureDef.friction = 0.1f;
-        fixtureDef.restitution = 0.9f; // Make it bounce a little bit
-
-// Create our fixture and attach it to the body
-        Fixture fixture = body.createFixture(fixtureDef);
-
-// Remember to dispose of any shapes after you're done with them!
-// BodyDef and FixtureDef don't need disposing, but shapes do.
-        circle.dispose();
-
         Sprite sprite = textureAtlas.createSprite("ball_cannon");
-        //sprite.setPosition(body.getPosition().x*PTM_RATIO, body.getPosition().y*PTM_RATIO);
-        sprite.setSize(radius*2, radius*2);
-        //worldBodies.add(body);
-
-        body = createBody("ball_cannon", xPos/PTM_RATIO, yPos/PTM_RATIO, 0);
-
+        sprite.setScale(SCALE);
+        sprite.setOrigin(0, 0);
+        body = createBody("ball_cannon", xPos, yPos, 0);
+        body.setLinearVelocity(200.0f, 20.0f);
+        body.setAngularVelocity(100.0f/radius);
         prosjektil = new Projectile(body, sprite, radius*2, radius*2, new Vector2(100,100));
 
-        body.setLinearVelocity(10.0f, 0.0f);
     }
 
 
@@ -203,5 +150,9 @@ public class MockGameWorld {
 
     public int getPTM_RATIO() {
         return PTM_RATIO;
+    }
+
+    public static float getSCALE() {
+        return SCALE;
     }
 }
--- core/src/views/game_world/GameWorldDrawer.java
+++ core/src/views/game_world/GameWorldDrawer.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
@@ -33,11 +34,12 @@ public class GameWorldDrawer extends Drawer {
     private World physicsWorld;
     private OrthographicCamera camera;
     private ExtendViewport viewport;
+    private float SCALE;
 
 
     private int PTM_ratio;
 
-    Box2DDebugRenderer debugRenderer;
+    Box2DDebugRenderer debugRenderer = new Box2DDebugRenderer();
 
 
     public GameWorldDrawer(SpriteBatch batch, MockGameWorld world){
@@ -45,10 +47,12 @@ public class GameWorldDrawer extends Drawer {
         this.mockWorld = world;
         this.physicsWorld = mockWorld.getPhysicsWorld();
         PTM_ratio = mockWorld.getPTM_RATIO();
+        SCALE = world.getSCALE();
+
 
         camera = new OrthographicCamera();
         viewport = new ExtendViewport(CastleCrush.WIDTH, CastleCrush.HEIGHT, camera);
-        viewport.update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), true);
+        viewport.update(CastleCrush.WIDTH, CastleCrush.HEIGHT, true);
 
         batch.setProjectionMatrix(camera.combined);
     }
@@ -69,21 +73,26 @@ public class GameWorldDrawer extends Drawer {
         drawObject(mockWorld.getProsjektil());
 
         batch.end();
+        debugRenderer.render(physicsWorld,camera.combined);
 
         mockWorld.getPhysicsWorld().step(1/60f, 6, 2);
     }
 
     private void drawObject(Drawable object) {
-        //batch.draw(object.getDrawable(), object.getBody().getPosition().x, object.getBody().getPosition().y, object.getWidth(), object.getHeight());
-        object.getDrawable().setPosition(object.getBody().getPosition().x*PTM_ratio, object.getBody().getPosition().y*PTM_ratio);
+       // batch.draw(object.getDrawable(), object.getBody().getLocalCenter().x, object.getBody().getLocalCenter().y, object.getWidth(), object.getHeight());
+        Vector2 position = object.getBody().getPosition();
+        float degrees = (float) Math.toDegrees(object.getBody().getAngle());
+        object.getDrawable().setPosition(position.x, position.y);
+        object.getDrawable().setRotation(degrees);
         object.getDrawable().draw(batch);
     }
+
 
 
     @Override
     public void dispose() {
 
-      //  debugRenderer.dispose();
+        debugRenderer.dispose();
 
     }
 
