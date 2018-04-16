4d46c91ce7d89dd242aa613ef1f3916046562ecb
TrulsElg <trulsel@stud.ntnu.no>
2018-04-16 11:16:32 +0200

    Added speed limitation for boxes being destroyed

--- core/src/models/GameCollision.java
+++ core/src/models/GameCollision.java
@@ -1,5 +1,6 @@
 package models;
 
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
@@ -37,11 +38,19 @@ public class GameCollision implements ContactListener {
                 if (fa.getBody().getUserData() instanceof Projectile || fb.getBody().getUserData() instanceof Projectile){
                     //System.out.println("Prosjektil");
                     if (fa.getBody().getUserData() instanceof Box) {
-                        ((Box) fa.getBody().getUserData()).isHit(true);
-                        gameWorld.addBodyToDestroy(fa);
+                        // Speed limitation
+                        if (getSpeed(fb.getBody().getLinearVelocity())>5){
+                            ((Box) fa.getBody().getUserData()).isHit(true);
+                            gameWorld.addBodyToDestroy(fa);
+                            System.out.println(getSpeed(fb.getBody().getLinearVelocity()));
+                        }
                     } else if (fb.getBody().getUserData() instanceof Box) {
-                        ((Box) fa.getBody().getUserData()).isHit(true);
-                        gameWorld.addBodyToDestroy(fb);
+                        // Speed limitation
+                        if (getSpeed(fa.getBody().getLinearVelocity())>5){
+                            ((Box) fa.getBody().getUserData()).isHit(true);
+                            gameWorld.addBodyToDestroy(fb);
+                            System.out.println(getSpeed(fa.getBody().getLinearVelocity()));
+                        }
 
                     }
                 }
@@ -63,5 +72,12 @@ public class GameCollision implements ContactListener {
     @Override
     public void postSolve(Contact contact, ContactImpulse impulse) {
 
+    }
+
+    private double getSpeed (Vector2 vector){
+        double x = vector.x;
+        double y = vector.y;
+
+        return Math.sqrt(x*x+y*y);
     }
 }
