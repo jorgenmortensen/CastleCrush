b08d50e564dc92385117657e525f73c1f793a111
TrulsElg <trulsel@stud.ntnu.no>
2018-04-22 20:18:37 +0200

    Added comment to OneWayWall

--- core/src/models/entities/OneWayWall.java
+++ core/src/models/entities/OneWayWall.java
@@ -3,7 +3,7 @@ package models.entities;
 import com.badlogic.gdx.physics.box2d.Body;
 
 public class OneWayWall {
-
+//    Class used for invisible walls that prevent objects other than a Projectile to move past it
     private Body body;
 
     public OneWayWall(Body body){
